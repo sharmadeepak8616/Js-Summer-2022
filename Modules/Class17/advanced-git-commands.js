e67bff715bc1d9b3a92a8ab0ae659b9d29bc8d73
Deepak Sharma <sharma.deepak8616@gmail.com>
2022-08-03 20:33:02 -0400

    Adde git commands

--- Modules/Class17/advanced-git-commands.js
+++ Modules/Class17/advanced-git-commands.js
@@ -4,20 +4,53 @@
  * 
  * 
  * To pull the code files from remote-branch into local-branch
- *  git pull
+ *      git pull
  * 
  * To view the local-branchNames
- *  git branch
+ *      git branch
  * 
  * Note: In results, * is present with the working branch
  * 
  * To create a new branch
- *  git branch newBranchName
+ *      git branch newBranchName
  * 
  * To move/checkout/switchTo on different branch
- *  git checkout branchName
+ *      git checkout branchName
  * 
  * To push a newly created local branch into remote (so that a corresponding remote-branch is also created)
- *  git push -u origin newBranchName
+ *      git push -u origin newBranchName
+ * 
+ * To delete a branch (in local)
+ *      git branch -d branchName
+ * 
+ */
+
+/**
+ * Steps:
+ * 
+ * Create a repo in github (js-wdio-summer-2022)
+ * Invite lab instructors as collaborator
+ * Clone the repo in local
+ * 
+ * 
+ * Open terminal
+ * Make sure that you are on master or main branch
+ * Perform "git pull" to pull code from remote-master to local-master
+ * Create new branch (using git branch newBranchName)
+ * Checkout on the new branch (using git checkout newBranchName)
+ * Push newBranch into remote (using git push -u origin newBranchName)
+ * Start working.....
+ * 
+ * Once done:
+ * Push all relevant code files in git (using git add/commit/push)
+ * Create a merge request and assign lab instructor as review
+ * 
+ * --> Lab instructors will merge your code in to master.
+ * 
+ * Once code is merged in master:
+ * Open terminal
+ * Go in to repo-folder
+ * Make sure you are on master/main branch (if not, checkout to master/main branch)
+ * Pull the code files from remote-master into local-master (using git pull)
  * 
  */
